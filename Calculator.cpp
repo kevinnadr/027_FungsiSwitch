2be846ef96ce05fe0998125fc91a1b5f952c1d74
kevinnadr <kevin19305.ib@gmail.com>
2024-03-26 17:59:33 +0700

    masukan variable pilihan

--- Calculator.cpp
+++ Calculator.cpp
@@ -31,3 +31,28 @@ float pembagian(float a, float b)
 {
     return a / b; 
 }
+
+float pembagian(float a, float b)
+{
+    return a / b; 
+}
+
+int main ()
+{
+    int pilihan;
+    do
+    {
+        cout << "kalkulator sederhana" << endl;
+        cout << "-===================" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. pengurangan" << endl;
+        cout << "3. perkalian" << endl;
+        cout << "4. pembagian" << endl;
+        cout << "5. keluar" << endl;
+        cout << "====================" << endl;
+        cout << "Masukan menu pilihan : ";
+        cin >> pilihan ;
+        
+        switch (pilihan)
+    }
+}
